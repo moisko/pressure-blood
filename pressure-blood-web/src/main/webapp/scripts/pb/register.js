15847c8b0f9835a12ec4714b9ddd26c0e4eed2e0
Boris Borisov <moisko@gmail.com>
2014-10-25 10:59:17 +0300

    separation of concerns
    Change-Id: I8d7ce644174c651ed19c3abf16c5b91cefa952de

--- pressure-blood-web/src/main/webapp/scripts/pb/register.js
+++ pressure-blood-web/src/main/webapp/scripts/pb/register.js
@@ -1,40 +1,36 @@
 var Register = {
-		registerUser : function() {
-			$.ajax({
-				url : "/pressure-blood-web/o.registerUser",
-				type : "POST",
-				dataType : "json",
-				contentType : "application/json; charset=utf-8",
-				data : JSON.stringify({
-					"username" : $("#username").val(),
-					"password" : $("#password").val(),
-					"confirmedPassword" : $("#confirmedPassword").val(),
-					"email" : $("#email").val()
-				}),
-				success : function(json) {
-					var status = json.status;
-					switch (status) {
-						case "SUCCESS" :
-							window.location.href = "/pressure-blood-web/index.jsp";
-							break;
-						case "EXISTS":
-							alert(json.message);
+	registerUser : function() {
+		$.ajax({
+			url : "/pressure-blood-web/o.registerUser",
+			type : "POST",
+			dataType : "json",
+			contentType : "application/json; charset=utf-8",
+			data : JSON.stringify({
+				"username" : RegisterForm.getUsername(),
+				"password" : RegisterForm.getPassword(),
+				"confirmedPassword" : RegisterForm.getConfirmedPassword(),
+				"email" : RegisterForm.getEmail()
+			}),
+			success : function(json) {
+				var status = json.status;
+				switch (status) {
+				case "SUCCESS":
+					window.location.href = "/pressure-blood-web/index.jsp";
+					break;
+				case "EXISTS":
+					alert(json.message);
 
-							$("#username").val("");
-							$("#password").val("");
-							$("#confirmedPassword").val("");
-							$("#email").val("");
-							break;
-					}
-				},
-				error : function(xhr, status) {
-					alertl("Failed to register user.\nServer returned " + xhr.status);
+					RegisterForm.clear();
 
-					$("#username").val("");
-					$("#password").val("");
-					$("#confirmedPassword").val("");
-					$("#email").val("");
+					break;
 				}
-			});
-		}
+			},
+			error : function(xhr, status) {
+				alert("Failed to register user.\nServer returned "
+						+ xhr.status);
+
+				RegisterForm.clear();
+			}
+		});
+	}
 }
