a765a7639c3cf915886dc2ba2c165e618815d531
Boris Borisov <moisko@gmail.com>
2014-12-22 18:31:09 +0200

    formatting
    Change-Id: I372e2bacb257f0ca306308d54e09a37d41409cad

--- pressure-blood-web/src/main/webapp/scripts/pb/time/date-bg.js
+++ pressure-blood-web/src/main/webapp/scripts/pb/time/date-bg.js
@@ -2,13 +2,13 @@
 jQuery.extend(jQuery.fn.dataTableExt.oSort, {
 	"date-bg-asc" : function(dateTimeStringA, dateTimeStringB) {
 		var dateTimeInMilllisA = LocalDateTime.parse(dateTimeStringA),
-		dateTimeInMilllisB = LocalDateTime.parse(dateTimeStringB);
+			dateTimeInMilllisB = LocalDateTime.parse(dateTimeStringB);
 		return dateTimeInMilllisA - dateTimeInMilllisB;
 	},
 
 	"date-bg-desc" : function(dateTimeStringA, dateTimeStringB) {
 		var dateTimeInMilllisA = LocalDateTime.parse(dateTimeStringA),
-		dateTimeInMilllisB = LocalDateTime.parse(dateTimeStringB);
+			dateTimeInMilllisB = LocalDateTime.parse(dateTimeStringB);
 		return dateTimeInMilllisA - dateTimeInMilllisB;
 	}
 });
