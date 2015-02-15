e73ad3ed4ab40eb699ee527b4f3d71021a98fd57
moisko <moisko@gmail.com>
2015-02-15 19:27:58 +0200

    fix registration
    Change-Id: I2dca4696d984b544eb869bb6fb5fadff74ab9b42

--- pressure-blood-web/src/main/webapp/scripts/reg.js
+++ pressure-blood-web/src/main/webapp/scripts/reg.js
@@ -3,16 +3,20 @@ require.config({
 		jquery : "jquery/jquery-1.11.1.min",
 		Register : "pb/Register",
 		RegisterForm : "pb/form/RegisterForm",
-		validate : "jquery/jquery.validate.min"
+		validate : "jquery/jquery.validate.min",
+		jqueryblockUI : "jquery/jquery.blockUI"
 	},
 	shim : {
 		validate : {
+			deps : ["jquery"]
+		},
+		jqueryblockUI : {
 			deps : ["jquery"]
 		}
 	}
 });
 
-require(["jquery", "Register", "RegisterForm", "validate"], function($, Register, RegisterForm) {
+require(["jquery", "Register", "RegisterForm", "validate", "jqueryblockUI"], function($, Register, RegisterForm) {
 	$("#register-form").submit(function(event) {
 		RegisterForm.validateRegisterForm();
 		if (RegisterForm.isRegisterFormValid()) {
